123ec1a285aaa54ba9650c89257c573d0e62beee
Mohummad Awais <74554508+gadamac@users.noreply.github.com>
2024-05-28 10:22:05 +0500

    Add files via upload

--- q1.js
+++ q1.js
@@ -1,7 +1,18 @@
-function convert()
+function calculate()
 {
-    let temp1= document.getElementById("n1").value;
-    let temp2= (temp1*9/5)+32;
-    document.getElementById("result").innerHTML= `${temp2}`;
-
+    var value1=document.getElementById("v1").value;
+    var value2=(value1*9/5)+32;
+    document.getElementById("result").innerHTML= value2;
 }
+//question no 2//
+let list = document.getElementById("world-cup-years");
+let listItem;
+
+for (let year = 2022; year <= 2050; year += 4) {
+	listItem = document.createElement("LI");
+	listItem.textContent = year;
+	list.appendChild(listItem);
+}
+
+
+
